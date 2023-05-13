3cac622f32d8321c8a3c110aa48db52c53d0e8eb
PatrikRacsko <patrik.racsko@hyperia.sk>
2023-05-13 22:58:49 +0200

    test: add service test

---
+++ src/services/userService/userService.spec.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect, beforeAll, vi, afterEach } from 'vitest'
+import { UserService } from './userService'
+import {HTTPClient} from '../http'
+
+
+describe.only('userService', () => {
+    beforeAll(() => {
+        HTTPClient.post = vi.fn().mockResolvedValue({ data: { token: '1234' } });
+    });
+    
+      afterEach(() => {
+        vi.clearAllMocks()
+      })
+  it('login function works properly', async () => {
+    const spy = vi.spyOn(HTTPClient, 'post');
+    await UserService.login('12334', '1234');
+    expect(spy).toHaveBeenCalled();
+  })
+})
