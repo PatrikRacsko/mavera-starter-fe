1a52493f2340301f7e475b8fa9d019120f49c22e
PatrikRacsko <patrik.racsko@hyperia.sk>
2023-05-13 22:58:21 +0200

    feat: add services

---
+++ src/services/http.ts
@@ -0,0 +1,31 @@
+import axios, {
+    type AxiosInstance,
+    type AxiosError,
+    type InternalAxiosRequestConfig,
+    type AxiosResponse
+  } from 'axios'
+  
+  export const HTTPClient: AxiosInstance = axios.create({
+    baseURL: import.meta.env.BASE_URL
+  })
+  
+  HTTPClient.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+      // TODO check local storage if user is logged in if yes add authorization header
+      return config
+    },
+    (error) => {
+      console.error(error)
+      return Promise.reject(error)
+    }
+  )
+  
+  HTTPClient.interceptors.response.use(
+    (response: AxiosResponse) => {
+      return response
+    },
+    (error: AxiosError) => {
+      // TODO Sentry or other bug tracking tool or redirects
+      return Promise.reject(error)
+    }
+  )
---
+++ src/services/userService/userService.ts
@@ -0,0 +1,8 @@
+import { HTTPClient } from "../http";
+
+export const UserService = {
+    async login(email: string, password: string) {
+        const response = await HTTPClient.post('/login', { email, password })
+        return response.data
+    }
+}
