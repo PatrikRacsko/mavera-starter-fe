c36f8ce0dc4f32da42a4e98e037ff402e6e91306
PatrikRacsko <patrik.racsko@hyperia.sk>
2023-05-13 23:03:25 +0200

    test: husky

--- src/services/userService/userService.spec.ts
+++ src/services/userService/userService.spec.ts
@@ -1,19 +1,18 @@
 import { describe, it, expect, beforeAll, vi, afterEach } from 'vitest'
 import { UserService } from './userService'
-import {HTTPClient} from '../http'
-
+import { HTTPClient } from '../http'
 
 describe('userService', () => {
-    beforeAll(() => {
-        HTTPClient.post = vi.fn().mockResolvedValue({ data: { token: '1234' } });
-    });
+  beforeAll(() => {
+    HTTPClient.post = vi.fn().mockResolvedValue({ data: { token: '1234' } })
+  })
 
-      afterEach(() => {
-        vi.clearAllMocks()
-      })
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
   it('login function works properly', async () => {
-    const spy = vi.spyOn(HTTPClient, 'post');
-    await UserService.login('12334', '1234');
-    expect(spy).toHaveBeenCalled();
+    const spy = vi.spyOn(HTTPClient, 'post')
+    await UserService.login('12334', '1234')
+    expect(spy).toHaveBeenCalled()
   })
 })
